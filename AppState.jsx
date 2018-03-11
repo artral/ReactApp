6f082106aac66188a4d17288714747a27c7c7607
Artralarasu Velusamy <artralarasu@gmail.com>
2018-03-11 11:48:36 -0600

    router with v4 update

--- AppState.jsx
+++ AppState.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import propTypes from 'prop-types'
 import ReactDOM from 'react-dom';
-import { Route, Router, Link, browserHistory, IndexRoute } from 'react-router';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 class Appstate1 extends React.Component {
     constructor() {
@@ -213,18 +214,24 @@ class RowComponent extends React.Component {
 
 
 class AppRouter extends React.Component {
-
     render() {
         return (
-            <div>
-                <ul>
-                    <li> Home...</li>
-                    <li> About....</li>
-                    <li> Contact....</li>
-                </ul>
-                {this.props.Children}
-            </div>
-        )
+            <Router>
+                <div>
+                    <h2>Welcome to React Router Tutorial</h2>
+                    <ul>
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/Contact'}>Contact</Link></li>
+                    </ul>
+                    <hr />
+
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/Contact' component={Contact} />
+                    </Switch>
+                </div>
+            </Router>
+        );
     }
 }
 export default AppRouter;
--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App.jsx';
+    import App from './App.jsx';
 import AppStateEx from './AppState.jsx';
+import AppRouter from './AppState.jsx';
+
+
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Home, About, Contact } from './AppState.jsx';
 
 ReactDOM.render(<AppStateEx />, document.getElementById('app'));
-ReactDOM.render(<AppStateEx />, document.getElementById('app1'));
-ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={AppAppRouter}>
-            <IndexRoute component={Home} />
-            <Route path="home" component={Home} />
-            <Route path="about" component={About} />
-            <Route path="contact" component={Contact} />
-        </Route>
-    </Router>
-), document.getElementById('app'))
+
